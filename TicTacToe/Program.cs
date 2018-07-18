c361fc44445df76b56467ac9eea15dd55f440e31
bartvdput <bartvdput@gmail.com>
2018-07-18 23:15:51 +0200

    added comments

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,49 +4,52 @@ using System.Linq;
 
 namespace TicTacToe
 {
-    class Program
-    {
+    class Program {
         private static Boolean?[,] _gameGrid;
         private static bool _player;
         private static bool _playing;
 
-        static void Main(string[] args)
-        {
+        /// <summary>
+        /// Initialize three variables and start the main game loop.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) {
             _player = true;
             _playing = false;
             _gameGrid = new Boolean?[3, 3] {{null, null, null}, {null, null, null}, {null, null, null}};
             GameLoop();
         }
 
-        private static void GameLoop()
-        {
+        /// <summary>
+        /// Main loop for starting a new game of Tic Tac Toe.
+        /// Ask if a new game has to be started, check input for the answer.
+        /// </summary>
+        private static void GameLoop() {
             bool loop = true;
-            do
-            {
+            do {
                 Console.WriteLine("Do you want to play a game of TicTacToe? [y/n]");
                 String response = Console.ReadLine();
 
-                if (response == "y")
-                {
+                if (response == "y") {
                     ClearGrid();
                     PlayGame();
                 }
-                else if (response == "n")
-                {
+                else if (response == "n") {
                     loop = false;
                 }
-                else
-                {
+                else {
                     Console.WriteLine("Please enter 'y' or 'n'.");
                 }
             } while (loop);
         }
 
-        private static void PlayGame()
-        {
+        /// <summary>
+        /// Loop for playing a single game of Tic Tac Toe.
+        /// Ask each player for the position of their next move.
+        /// </summary>
+        private static void PlayGame() {
             _playing = true;
-            do
-            {
+            do {
                 PrintGrid();
                 Console.WriteLine(_player ? "Turn of player 'o': " : "Turn of player 'x': ");
                 NewMove();
@@ -64,43 +67,40 @@ namespace TicTacToe
             Console.WriteLine(Environment.NewLine + Environment.NewLine + Environment.NewLine);
         }
 
-        private static void NewMove()
-        {
+        /// <summary>
+        /// Process a single move of a player.
+        /// Ask for the position and check if this position is valid (empty and within range).
+        /// </summary>
+        private static void NewMove() {
             bool moveCompleted = false;
-            do
-            {
+            do {
                 Console.WriteLine("What column do you want to mark? [0,1,2]");
                 String col = Console.ReadLine();
                 Console.WriteLine("What row do you want to mark? [0,1,2]");
                 String row = Console.ReadLine();
 
-                if (Int32.TryParse(col, out int c) && Int32.TryParse(row, out int r))
-                {
-                    if (_gameGrid[c, r] == null)
-                    {
+                if (Int32.TryParse(col, out int c) && Int32.TryParse(row, out int r)) {
+                    if (_gameGrid[c, r] == null) {
                         _gameGrid[c, r] = _player;
                         moveCompleted = true;
                     }
-                    else
-                    {
+                    else {
                         Console.WriteLine("Please choose an empty grid cell.");
                     }
                 }
-                else
-                {
+                else {
                     Console.WriteLine("Please enter values between 0 and 2.");
                 }
             } while (!moveCompleted);
-
         }
 
-        static void PrintGrid()
-        {
+        /// <summary>
+        /// Print the current grid to the console.
+        /// </summary>
+        static void PrintGrid() {
             Console.WriteLine("");
-            for (int i = 0; i < _gameGrid.GetLength(0); i++)
-            {
-                for (int j = 0; j < _gameGrid.GetLength(1); j++)
-                {
+            for (int i = 0; i < _gameGrid.GetLength(0); i++) {
+                for (int j = 0; j < _gameGrid.GetLength(1); j++) {
                     if (_gameGrid[j, i] == true)
                         Console.Write("o    ");
                     else if (_gameGrid[j, i] == false)
@@ -108,24 +108,27 @@ namespace TicTacToe
                     else
                         Console.Write("-    ");
                 }
-
                 Console.WriteLine("\n");
             }
         }
 
-        static void ClearGrid()
-        {
-            for (int i = 0; i < _gameGrid.GetLength(0); i++)
-            {
-                for (int j = 0; j < _gameGrid.GetLength(1); j++)
-                {
+        /// <summary>
+        /// Clear the grid for when a new game has to be started.
+        /// </summary>
+        static void ClearGrid() {
+            for (int i = 0; i < _gameGrid.GetLength(0); i++) {
+                for (int j = 0; j < _gameGrid.GetLength(1); j++) {
                     _gameGrid[j, i] = null;
                 }
             }
         }
 
-        private static Boolean CheckForWinner()
-        {
+        /// <summary>
+        /// Check if the player that has the turn is the winner.
+        /// All possible victory conditions are hardcoded, as there are only 8 of these.
+        /// </summary>
+        /// <returns>TRUE if current player has won. FALSE otherwise</returns>
+        private static Boolean CheckForWinner() {
             Boolean?[] winner = new Boolean?[] {_player, _player, _player};
             if (new Boolean?[] {_gameGrid[0, 0], _gameGrid[1, 0], _gameGrid[2, 0]}.SequenceEqual(winner))
                 return true;
@@ -147,6 +150,10 @@ namespace TicTacToe
                 return false;
         }
 
+        /// <summary>
+        /// Check if the grid is full. As this check occurrs after the check for a winnner, a full grid means a draw.
+        /// </summary>
+        /// <returns>TRUE if the game is a draw. FALSE otherwise.</returns>
         private static Boolean CheckforDraw() {
             for (int i = 0; i < _gameGrid.GetLength(0); i++) {
                 for (int j = 0; j < _gameGrid.GetLength(1); j++) {
